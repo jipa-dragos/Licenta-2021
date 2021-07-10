1a5c6df1a86a5f3e160aaa47d7d16e4e9e21ae64
Jipa Dragos <jipadragos09@gmail.com>
2021-07-10 15:18:16 +0300

    Add existing fields of the quiz in the update form.list

--- frontend/src/Components/pages/QuizUpdate.js
+++ frontend/src/Components/pages/QuizUpdate.js
@@ -3,9 +3,19 @@ import { useHttpClient } from '../../shared/hooks/http-hook'
 import { useParams } from 'react-router'
 import { Redirect, useHistory } from 'react-router-dom'
 import LoadingSpinner from '../../shared/components/UI/LoadingSpinner'
-import { Form, Input, Select, Button, DatePicker } from 'antd'
+import {
+  Form,
+  Input,
+  Button,
+  DatePicker,
+  Space,
+  Checkbox,
+  Select,
+  InputNumber,
+} from 'antd'
 import 'antd/dist/antd.css'
 import moment from 'moment'
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons'
 
 const layout = {
   labelCol: {
@@ -14,6 +24,7 @@ const layout = {
 }
 
 const { RangePicker } = DatePicker
+const { TextArea } = Input
 
 function disabledDate(current) {
   // Can not select days before today and today
@@ -55,9 +66,6 @@ function QuizUpdate() {
 
   const onFinish = (values) => {
     console.log(values.quiz)
-
-    console.log(values.rangeTime[0]._d)
-    console.log(values.rangeTime[1]._d)
     setRedirect(true)
   }
 
@@ -107,7 +115,7 @@ function QuizUpdate() {
               </Select>
             </Form.Item>
             <Form.Item
-              name='rangeTime'
+              name={['quiz', 'rangeTime']}
               label='time'
               initialValue={[
                 moment(loadedQuiz.data.startDate),
@@ -127,6 +135,175 @@ function QuizUpdate() {
                 ]}
               />
             </Form.Item>
+
+            <Form.List name='quizQuestion' initialValue={loadedQuiz.data.quiz}>
+              {(fields2, { add, remove }) => (
+                <>
+                  {fields2.map((field2) => (
+                    <React.Fragment key={field2.key}>
+                      <Space
+                        style={{
+                          display: 'flex',
+                          marginBottom: 8,
+                          marginLeft: 330,
+                        }}
+                        align='baseline'
+                      >
+                        <Form.Item
+                          {...field2}
+                          wrapperCol={{
+                            span: 20,
+                            offset: 6,
+                          }}
+                          name={[field2.name, 'question']}
+                          fieldKey={[field2.fieldKey, 'question']}
+                          rules={[
+                            {
+                              required: true,
+                              message: 'Missing question',
+                            },
+                          ]}
+                        >
+                          <TextArea
+                            style={{ width: 1000 }}
+                            rows={1}
+                            size='large'
+                            placeholder='q/a'
+                          />
+                        </Form.Item>
+
+                        <Form.Item
+                          {...field2}
+                          wrapperCol={{
+                            span: 23,
+                            offset: 16,
+                          }}
+                          style={{ width: 400, marginLeft: -150 }}
+                          name={[field2.name, 'tag']}
+                          fieldKey={[field2.fieldKey, 'tag']}
+                          rules={[
+                            {
+                              required: true,
+                              message: 'Missing tag',
+                            },
+                          ]}
+                        >
+                          <Input size='small' placeholder='TAG' />
+                        </Form.Item>
+
+                        <MinusCircleOutlined
+                          style={{ marginLeft: 10 }}
+                          onClick={() => remove(field2.name)}
+                        />
+                      </Space>
+                      <Form.List name={[field2.name, 'answers']}>
+                        {(answers, { add, remove }) => {
+                          return (
+                            <>
+                              {answers.map((answer) => (
+                                <Space key={answer.key} align='baseline'>
+                                  <Form.Item
+                                    {...answer}
+                                    wrapperCol={{
+                                      span: 12,
+                                      offset: 11,
+                                    }}
+                                    style={{ width: 1000, marginLeft: 30 }}
+                                    name={[answer.name, 'text']}
+                                    fieldKey={[answer.fieldKey, 'text']}
+                                    rules={[
+                                      {
+                                        required: true,
+                                        message: 'Missing answer',
+                                      },
+                                    ]}
+                                  >
+                                    <Input placeholder='answer' />
+                                  </Form.Item>
+
+                                  <Form.Item
+                                    name={[answer.name, 'isCorrect']}
+                                    valuePropName='checked'
+                                    noStyle
+                                    initialValue={false}
+                                  >
+                                    <Checkbox>Correct</Checkbox>
+                                  </Form.Item>
+
+                                  <p
+                                    style={{ marginLeft: 40, marginRight: -40 }}
+                                  >
+                                    Points
+                                  </p>
+                                  <Form.Item
+                                    {...answer}
+                                    wrapperCol={{
+                                      span: 12,
+                                      offset: 11,
+                                    }}
+                                    name={[answer.name, 'points']}
+                                    fieldKey={[answer.fieldKey, 'points']}
+                                    rules={[
+                                      {
+                                        required: true,
+                                        message: 'Missing points',
+                                      },
+                                    ]}
+                                    initialValue={0}
+                                  >
+                                    <InputNumber min={0} max={10} />
+                                  </Form.Item>
+
+                                  <MinusCircleOutlined
+                                    style={{ marginLeft: 40 }}
+                                    onClick={() => {
+                                      remove(answer.name)
+                                    }}
+                                  />
+                                </Space>
+                              ))}
+
+                              <Form.Item
+                                wrapperCol={{
+                                  span: 12,
+                                  offset: 6,
+                                }}
+                              >
+                                <Button
+                                  type='dashed'
+                                  onClick={() => {
+                                    add()
+                                  }}
+                                  block
+                                >
+                                  <PlusOutlined /> Add Answer
+                                </Button>
+                              </Form.Item>
+                            </>
+                          )
+                        }}
+                      </Form.List>
+                    </React.Fragment>
+                  ))}
+                  <Form.Item
+                    wrapperCol={{
+                      span: 12,
+                      offset: 6,
+                    }}
+                  >
+                    <Button
+                      type='default'
+                      onClick={() => add()}
+                      block
+                      icon={<PlusOutlined />}
+                    >
+                      Add Question
+                    </Button>
+                  </Form.Item>
+                </>
+              )}
+            </Form.List>
+
             <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
               <Button type='primary' htmlType='submit'>
                 Submit
