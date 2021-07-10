3c8c4e29c26aeb4464f74a5fa680e7b9086d1de9
Jipa Dragos <jipadragos09@gmail.com>
2021-07-10 14:39:59 +0300

    update startDate & endDate for quiz

--- frontend/src/Components/pages/QuizUpdate.js
+++ frontend/src/Components/pages/QuizUpdate.js
@@ -3,13 +3,21 @@ import { useHttpClient } from '../../shared/hooks/http-hook'
 import { useParams } from 'react-router'
 import { Redirect, useHistory } from 'react-router-dom'
 import LoadingSpinner from '../../shared/components/UI/LoadingSpinner'
-import { Form, Input, Select, Button } from 'antd'
+import { Form, Input, Select, Button, DatePicker } from 'antd'
 import 'antd/dist/antd.css'
+import moment from 'moment'
 
 const layout = {
   labelCol: {
     span: 8,
   },
+}
+
+const { RangePicker } = DatePicker
+
+function disabledDate(current) {
+  // Can not select days before today and today
+  return current && current < moment().add(-1, 'days')
 }
 
 function QuizUpdate() {
@@ -46,15 +54,17 @@ function QuizUpdate() {
   }, [sendRequest, id])
 
   const onFinish = (values) => {
-    console.log(values)
+    console.log(values.quiz)
+
+    console.log(values.rangeTime[0]._d)
+    console.log(values.rangeTime[1]._d)
     setRedirect(true)
   }
 
   const courseTitle = (courses, quiz) => {
     let foundCourseTitle
     for (const course of courses) {
-      if (quiz.course === course._id) 
-        foundCourseTitle = course.title
+      if (quiz.course === course._id) foundCourseTitle = course.title
     }
 
     return foundCourseTitle
@@ -96,7 +106,27 @@ function QuizUpdate() {
                 ))}
               </Select>
             </Form.Item>
-
+            <Form.Item
+              name='rangeTime'
+              label='time'
+              initialValue={[
+                moment(loadedQuiz.data.startDate),
+                moment(loadedQuiz.data.endDate),
+              ]}
+            >
+              <RangePicker
+                disabledDate={disabledDate}
+                showTime={{ format: 'HH:mm' }}
+                format='YYYY-MM-DD HH:mm'
+                onFinish={onFinish}
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your range!',
+                  },
+                ]}
+              />
+            </Form.Item>
             <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
               <Button type='primary' htmlType='submit'>
                 Submit
@@ -104,10 +134,9 @@ function QuizUpdate() {
             </Form.Item>
           </Form>
           {console.log(loadedQuiz)}
-          {console.log(loadedCourses.data)}
         </div>
       )}
-      {redirect && <Redirect to={{ pathname: history.goBack() }} />}
+      {/* {redirect && <Redirect to={{ pathname: history.goBack() }} />} */}
     </>
   )
 }
