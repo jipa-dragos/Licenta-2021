3e93c7a3b63720603ca0d8c81ec2c1902b22192b
Jipa Dragos <jipadragos09@gmail.com>
2021-07-15 16:40:28 +0300

    Button for clearing canvas

--- frontend/src/Components/pages/Canvas.js
+++ frontend/src/Components/pages/Canvas.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import { Button } from 'antd'
+import React, { useRef } from 'react'
 import CanvasDraw from 'react-canvas-draw'
 
 function Canvas() {
+    const drawCanvas = useRef(null)
 
+    const handleClear = () => {
+        drawCanvas.current.clear()
+    }
 
   return (
     <>
+      <div>
+        <Button onClick={handleClear}>Clear Canvas</Button>
+      </div>
       <CanvasDraw
         brushRadius={0.5}
         brushColor='black'
         hideGrid={true}
+        ref={drawCanvas}
       />
     </>
   )
